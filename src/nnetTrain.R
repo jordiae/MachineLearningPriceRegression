5450c91c80ab42e513b1c418c5b3f53af43cf78d
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-01-07 13:32:35 +0100

    Create nnetTrain.R

---
+++ src/nnetTrain.R
@@ -0,0 +1,25 @@
+library(MASS)
+library(nnet)
+library(caret)
+set.seed(111)
+train.data <- read.csv("./dataset_train.csv")
+summary(train.data)
+
+## specify 5x5 CV
+trc <- trainControl (method="repeatedcv", number=5, repeats=5)
+
+(decays <- 10^seq(-3,0,by=0.05))
+
+## WARNING: this takes some time
+model.nnet <- train (medianHouseValue ~., data = train.data, method='nnet', maxit = 1500, linout = TRUE,
+                        tuneGrid = expand.grid(.size=25,.decay=decays), trControl=trc)
+
+####################################
+save(model.nnet, file = "NNet_5x5CV.mod")
+####################################
+
+## We can inspect the full results
+model.nnet$results
+
+## and the best model found
+model.nnet$bestTune
