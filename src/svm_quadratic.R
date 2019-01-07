5eea21677cd18e685d8a7efc69703c7bc7acdb42
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-01-07 15:15:40 +0100

    Create svm_quadratic.R

---
+++ src/svm_quadratic.R
@@ -0,0 +1,12 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+learn.data <- read.csv("../data/dataset_train.csv")
+set.seed(1234)
+
+library(e1071)
+
+model.SVMQuad <- tune(svm, medianHouseValue~., data = learn.data, kernel="polynomial", degree=2,coef0=1,
+                ranges = list(cost =10^seq(-2,3),epsilon=10^seq(-5,2)),
+                tunecontrol = tune.control(sampling = "cross",nrepeat=5), scale = FALSE
+)
+
+save(model.SVMQuad, file = "bestSVMQuad.mod")
