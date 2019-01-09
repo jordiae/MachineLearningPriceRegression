9067998fc1f9a1047a40e6f95dc34e70db014968
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-01-09 13:05:42 +0100

    Update LASSO_evaluation.R

--- src/LASSO_evaluation.R
+++ src/LASSO_evaluation.R
@@ -1,4 +1,4 @@
-load("../data/LASSO.mod")
+load("../models/LASSO.mod")
 
 plot(model.lasso)
 
@@ -7,4 +7,13 @@ coef(model.lasso)
 # lambda.min is the value of lambda that gives minimum mean cross-validated error
 model.lasso$lambda.min
 
-sqrt(model.lasso$cvm[model.lasso$lambda == model.lasso$lambda.min])
+model.mse <- model.lasso$cvm[model.lasso$lambda == model.lasso$lambda.min]
+
+# value for RMSE
+sqrt(model.mse)
+
+train.data <- read.csv("../data/dataset_train.csv")
+N <- nrow(train.data)
+
+# and the corresponding predictive R-square
+(R2 <- (1 - model.mse*N/((N-1)*var(train.data$medianHouseValue)))*100)
