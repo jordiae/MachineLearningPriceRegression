061346e0e91489bb1fa74576e22acebc288ef7ce
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-01-09 13:18:48 +0100

    Create svm_quadratic_results.R

---
+++ src/svm_quadratic_results.R
@@ -0,0 +1,15 @@
+load("../models/bestSVMQuad.mod")
+
+# Best parameters found for the quadratic SVM
+model.SVMQuad$best.parameters
+
+mse <- model.SVMQuad$best.performance
+
+# RMSE for quadratic SVM
+sqrt(mse)
+
+train.data <- read.csv("../data/dataset_train.csv")
+N <- nrow(train.data)
+
+# and the corresponding predictive R-square
+(R2 <- (1 - model.mse*N/((N-1)*var(train.data$medianHouseValue)))*100)
